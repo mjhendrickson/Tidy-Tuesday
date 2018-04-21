d01549b5346c6691c8a0d59cea0da061d475f2df
Matthew Hendrickson <mjhendrickson1@gmail.com>
2018-04-21 16:19:37 -0400

    new plots

--- Week 01 - US Tuition/us_tuition.R
+++ Week 01 - US Tuition/us_tuition.R
@@ -8,12 +8,16 @@
 install.packages("tidyverse")
 install.packages("readxl")
 install.packages("scales")
+install.packages("viridis")
+#install.packages("RColorBrewer")
 
 
 # ===== Load needed packages =====
 library(tidyverse)
 library(readxl)
 library(scales)
+library(viridis)
+#library(RColorBrewer)
 
 
 # ===== Load & review data =====
@@ -42,26 +46,64 @@ avg_tuit <- average_tuition %>% # append new fields to df
 # ===== Create plots =====
 # Average Tuition - Box
 avg_tuition %>% 
-  ggplot(aes(x = fct_reorder(state, tuition), y = tuition)) +
+  ggplot(aes(x = fct_reorder(state, tuition), 
+             y = tuition)) +
   geom_boxplot() +
   coord_flip() +
-  scale_y_continuous(labels = comma) +
+  scale_y_continuous(labels = scales::dollar) +
   #theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
-  labs(x = "State", y = "Tuition Range ($) 2004-05 - 2015-16",
-       title = "College Tuition by State",
+  labs(x = "State", 
+       y = "Tuition Range",
+       title = "College Tuition Ranges by State - Academic Years 2004-2005 - 2015-2016",
        caption = "\nDataSource: https://trends.collegeboard.org/ | Graphic: @mjhendrickson")
+
 
 # Average Tuition - Point
 avg_tuition %>% 
-  ggplot(aes(x = fct_reorder(state, tuition), y = tuition, color = year)) +
+  ggplot(aes(x = fct_reorder(state, tuition), 
+             y = tuition, color = year)) +
   geom_point() +
   coord_flip() +
-  scale_y_continuous(labels = comma) +
+  scale_y_continuous(labels = scales::dollar) +
   #theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
-  labs(x = "State", y = "Tuition Range ($) 2004-05 - 2015-16",
+  labs(x = "State", 
+       y = "Tuition",
+       color = "Academic Year", 
        title = "College Tuition by State",
        caption = "\nDataSource: https://trends.collegeboard.org/ | Graphic: @mjhendrickson")
 
 
+# Average Tuition & 5 Year Change - Point
+avg_tuit %>% 
+  ggplot(aes(x = fct_reorder(state, tuition), 
+             y = tuition, 
+             color = tuition_5yr_pct_chg, 
+             size = tuition_5yr_pct_chg)) +
+  geom_point() +
+  coord_flip() +
+  scale_y_continuous(labels = scales::dollar) +
+  scale_color_viridis() +
+  #theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+  labs(x = "State", 
+       y = "Average Tuition",
+       size = "5 yr change", 
+       color = "", 
+       title = "Average College Tuition by State - Academic Year 2015-2016",
+       caption = "\nDataSource: https://trends.collegeboard.org/ | Graphic: @mjhendrickson")
 
-# ===== Save plots =====
+
+# Average Tuition & 5 Year Change - Bar
+avg_tuit %>% 
+  ggplot(aes(x = fct_reorder(state_abb, tuition), 
+             y = tuition, 
+             fill = tuition_5yr_pct_chg)) +
+  geom_bar(stat = "Identity") +
+  geom_text(aes(label = round(tuition), angle = 90)) +
+  scale_y_continuous(labels = scales::dollar) +
+  scale_color_viridis() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+  labs(x = "State", 
+       y = "Average Tuition",
+       fill = "5 yr % change", 
+       title = "Average College Tuition by State - Academic Year 2015-2016",
+       caption = "\nDataSource: https://trends.collegeboard.org/ | Graphic: @mjhendrickson")
